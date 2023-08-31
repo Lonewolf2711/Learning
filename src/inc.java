866692a2bbfed1cf653e0f331334daca3dc7c4d0
Lonewolf <devaganthanb@yahoo.com>
2023-08-31 14:10:04 +0530

    updated

--- src/inc.java
+++ src/inc.java
@@ -1,8 +1,6 @@
 public class inc {
     public static void main(String[] args) {
-        int a=10;//11
-        System.out.println(a++ - a++ - --a - ++a - a--);//pre increment//
-        //System.out.println(--a);//pre decrement//10
+        int a=10;
 
     }
 }
